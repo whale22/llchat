34e8d06fc7b47984a86b5a4495348f2e06e67837
whale22 < realwhale64@naver.com>
2024-12-31 15:40:56 +0900

    DTO MessagesResponse 추가

--- src/main/java/com/ll/chat/chatting/ChatController.java
+++ src/main/java/com/ll/chat/chatting/ChatController.java
@@ -1,7 +1,9 @@
 package com.ll.chat.chatting;
 
+import com.ll.chat.chatting.DTO.MessagesResponse;
 import com.ll.chat.chatting.DTO.WriteMessageResponse;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -24,5 +26,11 @@ public class ChatController {
         chatMessages.add(ch2);
 
         return new RsData("200", "메세지가 성공적으로 작성되었습니다.", new WriteMessageResponse(chatMessages));
+    }
+
+    @GetMapping("/messages")
+    @ResponseBody
+    public RsData<List<ChatMessage>> messages() {
+        return new RsData("200","메세지가 성공적으로 로드되었습니다.", new MessagesResponse(chatMessages));
     }
 }
---
+++ src/main/java/com/ll/chat/chatting/DTO/MessagesResponse.java
@@ -0,0 +1,13 @@
+package com.ll.chat.chatting.DTO;
+
+import com.ll.chat.chatting.ChatMessage;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.List;
+
+@AllArgsConstructor
+@Getter
+public class MessagesResponse {
+    List<ChatMessage> chatMessages;
+}
